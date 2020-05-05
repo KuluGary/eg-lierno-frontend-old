9ccf5bab6801e195c0e40cd301e989e4257960c9
Gary <kululu.gary@gmail.com>
2020-05-05 19:13:13 +0200

    fixed url

--- src/helpers/api-config.js
+++ src/helpers/api-config.js
@@ -1,3 +1,3 @@
 export const dev = 'http://localhost:3001/api/v1';
 
-export const prod = 'https://pro-lierno-backend.herokuapp.com/api/v1/';
+export const prod = 'https://pro-lierno-backend.herokuapp.com/api/v1';
